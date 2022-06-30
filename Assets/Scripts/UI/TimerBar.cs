d78f38d9f19124e44a0b63559708dae0619ea2b1
Allan <couetteallan@gmail.com>
2022-06-30 10:14:38 +0200

    Score Panel + Timer Slider set up

---
+++ Assets/Scripts/UI/TimerBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerBar : MonoBehaviour
+{
+    public Gradient gradient;
+    private float timer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
